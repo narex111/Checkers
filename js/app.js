0cf6b60e773785e10f74bd8e87dc804f1a497636
Narek <narex111@gmail.com>
2019-07-05 19:14:40 -0700

    finished a boardMaker function that builds a board and assigns a 8x8 array matrix to game.rows

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 const game = {
-    rows: {},
+    rows: [],
     squares: {},
-    rowMaker(){
-        for(i=0; i<8; i++){
-            Object.defineProperty(this.rows, i+1, {value:[]})
+    boardMaker(){
+        for(let i=0; i<8; i++){
+            // Object.defineProperty(this.rows, "row" + (i+1), {value:[]})
+            this.rows.push([])
             if(i%2 === 0){
             const row = document.createElement("div")
             row.id = i+1
@@ -11,19 +12,58 @@ const game = {
             row.style.height = "12.5%"
             row.style.backgroundColor = "white"
             document.querySelector("#board").appendChild(row)
-            } else {
+            //square on even rows
+            for(let j=0; j<8; j++){
+                this.rows[i].push(0)
+                if (j%2 === 0){
+                const square = document.createElement("div")
+                square.id = row.id + (j+1)
+                square.style.width = "12.5%"
+                square.style.height = "100%"
+                square.style.backgroundColor = "black"
+                row.appendChild(square)
+                } else{
+                    const square = document.createElement("div")
+                square.id = row.id + (j+1)
+                square.style.width = "12.5%"
+                square.style.height = "100%"
+                square.style.backgroundColor = "white"
+                row.appendChild(square)
+                }
+            }
+
+        } else {
                 const row = document.createElement("div")
                 row.id = i+1
                 row.style.display = "flex"
                 row.style.height = "12.5%"
                 row.style.backgroundColor = "black"
                 document.querySelector("#board").appendChild(row)
+
+                for(let j=0; j<8; j++){
+                    this.rows[i].push(0)
+                    if (j%2 === 0){
+                    const square = document.createElement("div")
+                    square.id = row.id + (j+1)
+                    square.style.width = "12.5%"
+                    square.style.height = "100%"
+                    square.style.backgroundColor = "white"
+                    row.appendChild(square)
+                    } else{
+                        const square = document.createElement("div")
+                    square.id = row.id + (j+1)
+                    square.style.width = "12.5%"
+                    square.style.height = "100%"
+                    square.style.backgroundColor = "black"
+                    row.appendChild(square)
+                    }
+                }
             }
         }
         console.log(this.rows)
     },
     // squareMaker(){
-    //     for(i=0; i<8; i++){
+    //     for(let i=0; i<8; i++){
     //         if(i%2 === 0){
     //             const square = document.createElement("div")
     //             square.id = "letter"+i
@@ -31,6 +71,14 @@ const game = {
     //             square.style.height = "100%"
     //             square.style.backgroundColor = "black"
     //             document.querySelector(`#${i+1}`).appendChild(square)
+    //             for(i=0; i<8; i++){
+    //                     const square = document.createElement("div")
+    //                     square.id = "letter"+i
+    //                     square.style.width = "12.5%"
+    //                     square.style.height = "100%"
+    //                     square.style.backgroundColor = "black"
+    //                     document.querySelector(`#${i+1}`).appendChild(square)
+    //             }
     //         } else {
     //                 const square = document.createElement("div")
     //                 square.id = "letter"+i
@@ -44,5 +92,5 @@ const game = {
 }
 
 
-game.rowMaker()
-game.squareMaker()
+game.boardMaker()
+// game.squareMaker()
