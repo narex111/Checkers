7d90f6ede6da23a42d1d8a36d17baec4985ccbd5
Narek <narex111@gmail.com>
2019-07-09 22:40:14 -0700

    finished mover regular move and eat

--- js/game.js
+++ js/game.js
@@ -2,6 +2,8 @@ class Game {
     constructor(){
     this.rows = [];
     this.previousPossibles = [];
+    this.red = {};
+    this.blue = {};
     }
     boardMaker(){
         for(let i=0; i<8; i++){
@@ -118,30 +120,30 @@ class Game {
             for(let j=0; j<8; j++){
                 const square = row[j]
 //defining .red figures
-                const red = document.createElement("div")
-                red.className = "red"
-                red.style.width = "80%"
-                red.style.height = "80%"
-                red.style.backgroundColor = "red"
-                red.style.borderRadius = "50%"
-                red.style.alignSelf ="center"
-                red.style.justifySelf = "center"
+                this.red = document.createElement("div")
+                this.red.className = "red"
+                this.red.style.width = "80%"
+                this.red.style.height = "80%"
+                this.red.style.backgroundColor = "red"
+                this.red.style.borderRadius = "50%"
+                this.red.style.alignSelf ="center"
+                this.red.style.justifySelf = "center"
 //defining .blue figures
-                const blue = document.createElement("div")
-                blue.className = "blue"
-                blue.style.width = "80%"
-                blue.style.height = "80%"
-                blue.style.backgroundColor = "blue"
-                blue.style.borderRadius = "50%"
-                blue.style.alignSelf ="center"
-                blue.style.justifySelf = "center"
+                this.blue = document.createElement("div")
+                this.blue.className = "blue"
+                this.blue.style.width = "80%"
+                this.blue.style.height = "80%"
+                this.blue.style.backgroundColor = "blue"
+                this.blue.style.borderRadius = "50%"
+                this.blue.style.alignSelf ="center"
+                this.blue.style.justifySelf = "center"
 //appending red figures based on this.rows matrix values
                 if(this.rows[i-1][j] === 1){
-                    square.appendChild(red)
+                    square.appendChild(this.red)
                 } 
 //appending blue figures based on this.rows matrix values
                 else if(this.rows[i-1][j] === 2){
-                    square.appendChild(blue)
+                    square.appendChild(this.blue)
                 }
             }
         }
@@ -172,6 +174,17 @@ class Game {
         const i = id[0]
         const j = id[1]
         this.rows[i][j] = value
+    }
+
+    matrixToSquare(i,k){
+        if (this.rows[i][k] === 1){
+            document.getElementById(i.toString()+k.toString()).appendChild(this.red)
+        } else if(this.rows[i][k] === 2){
+            document.getElementById(i.toString()+k.toString()).appendChild(this.blue)
+        } else if(this.rows[i][k] === 0){
+            document.getElementById(i.toString()+k.toString()).removeChild(document.getElementById(i.toString()+k.toString()).firstChild)
+        }
+
     }
     //---------------------------------------
 
@@ -241,8 +254,8 @@ class Game {
             if (e.target.className === "blue" && e.target.id == ""){
 
 //cleaning previous "click" id's and "possible"'s both on the board and in the matrix
-        this.cleaner()
-        e.target.id = "clicked"
+            this.cleaner()
+            e.target.id = "clicked"
 //adding 3's with different scenarios
                 if(this.rows[i-1][j-1] === 0){
                     this.rows[i-1][j-1] = 3
@@ -259,7 +272,7 @@ class Game {
                 this.rows[i-1][j+1] = 3
                 document.getElementById((i-1).toString() + (j+1).toString()).className = "possible"
                 }
-                else if(this.rows[i-1][j+1] === 2 && this.rows[i-2][j+2] === 0){
+                else if(this.rows[i-1][j+1] === 1 && this.rows[i-2][j+2] === 0){
                     this.rows[i-2][j+2] = 3
                     document.getElementById((i-2).toString() + (j+2).toString()).className = "possible"
                 }
@@ -299,38 +312,48 @@ class Game {
                 }
             }
         }
-        // console.log(this.previousPossibles)
     }
 
     mover(){
 
 
         board.addEventListener("click", (e) =>{
-            const board = document.getElementById("board")
-            const possibles = document.getElementsByClassName("possible")
             const clicked = document.querySelector("#clicked")
             if (e.target.className === "possible"){
-                this.squareToMatrix(clicked.parentNode, 0)
-                e.target.appendChild(clicked)
                 if (clicked.className === "red"){
+//if we see blue in front of red, we delete blue before moving red to e.target 
+                    if(parseInt(e.target.id[0])-parseInt(clicked.parentNode.id[0]) === 2 && parseInt(e.target.id[1])>parseInt(clicked.parentNode.id[1])){
+                        this.rows[parseInt(clicked.parentNode.id[0])+1][parseInt(clicked.parentNode.id[1])+1] = 0
+                        this.matrixToSquare(parseInt(clicked.parentNode.id[0])+1, parseInt(clicked.parentNode.id[1])+1)
+                    }
+                    else if(parseInt(e.target.id[0])-parseInt(clicked.parentNode.id[0]) === 2 && parseInt(e.target.id[1])<parseInt(clicked.parentNode.id[1])){
+                        this.rows[parseInt(clicked.parentNode.id[0])+1][parseInt(clicked.parentNode.id[1])-1] = 0
+                        this.matrixToSquare(parseInt(clicked.parentNode.id[0])+1, parseInt(clicked.parentNode.id[1])-1)
+                    }
+                    //moving red to e.target
                     this.squareToMatrix(e.target, 1)
-                } else if(clicked.className === "blue"){
+                } 
+                
+                else if(clicked.className === "blue"){
+//if we see red in front of blue, we delete red before moving blue to e.target 
+                    if(parseInt(clicked.parentNode.id[0]-parseInt(e.target.id[0])) === 2 && parseInt(e.target.id[1])>parseInt(clicked.parentNode.id[1])){
+                        this.rows[parseInt(clicked.parentNode.id[0])-1][parseInt(clicked.parentNode.id[1])+1] = 0
+                        this.matrixToSquare(parseInt(clicked.parentNode.id[0])-1, parseInt(clicked.parentNode.id[1])+1)
+                    }
+                    else if(parseInt(clicked.parentNode.id[0]) - parseInt(e.target.id[0]) === 2 && parseInt(e.target.id[1])<parseInt(clicked.parentNode.id[1])){
+                        this.rows[parseInt(clicked.parentNode.id[0])-1][parseInt(clicked.parentNode.id[1])-1] = 0
+                        this.matrixToSquare(parseInt(clicked.parentNode.id[0])-1, parseInt(clicked.parentNode.id[1])-1)
+                    }
+                    //moving red to e.target
                     this.squareToMatrix(e.target, 2)
                 }
+                //cleaning where the figure used to be
+                this.squareToMatrix(clicked.parentNode, 0)
+                //moving the clicked figure to e.target
+                e.target.appendChild(clicked)
                 this.cleaner()
             }
         })
-    //     for (let k=0; k<8; k++){
-    //         for(let l=0; l<8; l++){
-    //             if(this.rows[k][l] === 3){
-
-
-    //             }
-    //             const fig = document.getElementById(k.toString()+l.toString()).firstChild
-    //             if (Boolean(fig) === true && fig.id === "clicked"){
-
-    //             }
-
     }
 
 
